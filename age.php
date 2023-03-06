0b61f17d7c81df68c0a301ad15a2edc71134209b
unknown <mirvatbarakat1@gmail.com>
2023-03-06 02:23:11 +0200

    Added a function to check wether a number is prime or not

---
+++ age.php
@@ -0,0 +1,14 @@
+<?php
+function is_prime($num)
+{
+    if($num == 1)
+        return false;
+    for($i=2;$i <$num;$i++)
+    {
+        if($num % $i == 0)
+            return false;
+    }
+    return true;
+}
+
+?>
