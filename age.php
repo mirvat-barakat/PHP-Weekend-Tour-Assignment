d5e76a11af3a293d1f20b019838b0495d1048aa2
unknown <mirvatbarakat1@gmail.com>
2023-03-06 02:42:52 +0200

    Added the input year of birth and result.

--- age.php
+++ age.php
@@ -10,5 +10,19 @@ function is_prime($num)
     }
     return true;
 }
+function is_even($num)
+{
+    if($num%2==0)
+        return true;
+    return false;
+}
+$yearofbirth = $_POST['yearofbirth'];
+$age = date('Y') - $yearofbirth;
 
+if(is_prime($yearofbirth) && is_even($yearofbirth))
+     echo "your age is prime and even.";
+if(!is_prime($yearofbirth) && is_even($yearofbirth))
+     echo "your age is even and not prime.";
+if(is_prime($yearofbirth) && !is_even($yearofbirth))
+     echo "your age is prime and not even.";
 ?>
